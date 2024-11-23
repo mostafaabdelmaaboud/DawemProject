27c57b9fbaeb22c68ec624c52b00fc10c17d6e83
Mostafa Abdelmaaboud <mostafa.abdelmaaboud@cashlinemena.com>
2024-11-23 12:02:53 +0200

    permissionV6

--- src/app/Presentation/user/permission-type/permission-type.module.ts
+++ src/app/Presentation/user/permission-type/permission-type.module.ts
@@ -5,6 +5,7 @@ import { PermissionTypeRoutingModule } from './permission-type-routing.module';
 import { PermissionTypeComponent } from './permission-type.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
     CommonModule,
     PermissionTypeRoutingModule,
     MatDialogModule,
+    TranslateModule,
     SharedModule
   ]
 })
